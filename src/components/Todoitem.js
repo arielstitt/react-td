aac40d9c062a349972ad499bddff649454168534
Ariel Stitt <arielstitt@Skills-Macbook-Pro-37.local>
2019-08-13 11:16:27 -0400

    added prop-types to compenents

---
+++ src/components/Todoitem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Todoitem extends Component {
+    render() {
+        return (
+            <div>
+                <p>{ this.props.todo.title }</p>
+            </div>
+        );
+    }
+}
+// PropTypes
+Todoitem.propTypes = {
+    todos: PropTypes.object.isRequired
+}
+
+export default Todoitem;
